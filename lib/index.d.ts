056c72a93ebcb88f161fc835c5e8177895db1b4a
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2021-09-22 22:00:20 +0530

    initial commit

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+export interface Token {
+  logo: string;
+  name: string;
+  symbol: string;
+  address: string;
+}
+
+export const DEFAULT_TOKEN_LIST: Array<Token>
+
+export const COMMON_BASES: Array<Token>
