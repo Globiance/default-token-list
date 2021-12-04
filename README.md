5f3d388952e810d2fb73e99fa39d17a4fcec1e90
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2021-12-04 18:09:10 +0530

    refactor & using mainnet addresses

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# default-token-list
+# GlobianceDEX - default-token-list
+
+This is the module which contains the list of tokens which will be available by default for swapping on [GlobianceDEX](https://dex.globiance.com) for everyone. This module also enlists the default set of pool visible in the [pool list](http://dex.globiance.com/pool).
+
+## Listing a token
+
+To request a new token listing you can do any of the following:
+
+1. Generate [new issue](https://github.com/Globiance/default-token-list/issues/new/choose) with all details
+2. Generate a Pull Request ( PR ) by add your logo ( as per [format](#token-logo-format) ) to [assets](https://github.com/Globiance/default-token-list/tree/master/src/assets) and add a JSON file containing details of the token in the [directory](https://github.com/Globiance/default-token-list/tree/master/src/tokens). 
+
+
+Format of the JSON file:
+
+* Filename must be token name in lowercase
+
+```
+{
+  "name": ,
+  "symbol": "",
+  "address": "",
+  "decimals": ,
+  "feeOnTransfer": 
+}
+```
+
+Sample:
+
+```
+{
+  "name": "Globiance Exchange Token",
+  "symbol": "GBEX",
+  "address": "xdc34514748f86a8da01ef082306b6d6e738f777f5a",
+  "decimals": 18,
+  "feeOnTransfer": true
+}
+```
+
+## Listing a Pool
+
+To enlist a pool in the default liquidity [pools list](http://dex.globiance.com/pool) add PR with corresponding pool details added in [directory](https://github.com/Globiance/default-token-list/tree/master/src/pools) 
+
+Format:
+
+* Filename must be pair address
+* all token names must be in lowercase
+
+File Structure:
+
+```
+{
+  "first": "",
+  "second": "",
+  "address": ""
+}
+```
+
+Sample:
+
+```
+{
+  "first": "gbex",
+  "second": "xdc",
+  "address": "xdc1aF38719EdEFE7D2aE5923198CCeea821968278F"
+}
+```
+
+
+## Token Logo Format
+
+Token logo needs to be in a .PNG format with preferrable dimensions of 50x50 px
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 
+const TOKENS_PATH = path.join(__dirname, "tokens");
+const BASES_PATH = path.join(__dirname, "bases");
+const POOLS_PATH = path.join(__dirname, "pools");
+
 function getAssetLogo(name) {
   return (
     "data:image/png;base64, " +
@@ -10,185 +14,65 @@ function getAssetLogo(name) {
   );
 }
 
-const DEFAULT_TOKEN_LIST = [
-  {
-    logo: getAssetLogo("srx"),
-    name: "StorX Network",
-    symbol: "TSRX",
-    address: "xdc55e574e7d0256ff50f86ee8cd90e4a8e22aa1f85",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("xdc"),
-    name: "XinFin Network",
-    symbol: "XDC",
-    address: "xdcc039850F937C623024DA66D6dF370022E6F16e30",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("gbex"),
-    name: "Globiance Network",
-    symbol: "GBEX",
-    address: "xdc2049552f4f55b8af48c86ed9866a66e7d2bcd0fc",
-    decimals: 18,
-    feeOnTransfer: true,
-  },
-  {
-    logo: getAssetLogo("eurg"),
-    name: "Euro Stablecoin",
-    symbol: "EURG",
-    address: "xdcb128a44f005cbe7ddaeb1860c16821842895ec7a",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("usdg"),
-    name: "USD Stablecoin",
-    symbol: "USDG",
-    address: "xdc7bd4983e237f351f2c94006097b2718403ff740f",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("sgdg"),
-    name: "SGD Stablecoin",
-    symbol: "SGDG",
-    address: "xdcd14ef8829e8bcb2c71d1b75fa6b1915680f089ed",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("gbpg"),
-    name: "GBP Stablecoin",
-    symbol: "GBPG",
-    address: "xdc17c92b3e3b3fa8dc8ae44c749fefab1adc5e3506",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("hkdg"),
-    name: "HKD Stablecoin",
-    symbol: "HKDG",
-    address: "xdce5c5e7e5eaacf28b2dd505a58c793c83a239ee4a",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("wxdc"),
-    name: "Wrapped XDC",
-    symbol: "WXDC",
-    address: "xdcc039850F937C623024DA66D6dF370022E6F16e30",
-    decimals: 18,
-  },
-];
+function getTokenFromSymbol(symbol) {
+  const token = JSON.parse(
+    fs.readFileSync(path.join(TOKENS_PATH, `${symbol}.json`)).toString()
+  );
+  const logo = getAssetLogo(token.symbol.toLowerCase());
+  return {
+    ...token,
+    logo,
+  };
+}
 
-const COMMON_BASES = [
-  {
-    logo: getAssetLogo("xdc"),
-    name: "XinFin Network",
-    symbol: "XDC",
-    address: "xdcc039850F937C623024DA66D6dF370022E6F16e30",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("wxdc"),
-    name: "Wrapped XDC",
-    symbol: "WXDC",
-    address: "xdcc039850F937C623024DA66D6dF370022E6F16e30",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("gbex"),
-    name: "Globiance Network",
-    symbol: "GBEX",
-    address: "xdc2049552f4f55b8af48c86ed9866a66e7d2bcd0fc",
-    decimals: 18,
-    feeOnTransfer: true,
-  },
-  {
-    logo: getAssetLogo("eurg"),
-    name: "Euro Stablecoin",
-    symbol: "EURG",
-    address: "xdcb128a44f005cbe7ddaeb1860c16821842895ec7a",
-    decimals: 18,
-  },
-  {
-    logo: getAssetLogo("usdg"),
-    name: "USD Stablecoin",
-    symbol: "USDG",
-    address: "xdc7bd4983e237f351f2c94006097b2718403ff740f",
-    decimals: 18,
-  },
-];
+function getAllTokens() {
+  return fs.readdirSync(TOKENS_PATH).map((x) => {
+    const token = JSON.parse(
+      fs.readFileSync(path.join(TOKENS_PATH, x)).toString()
+    );
+    const logo = getAssetLogo(token.symbol.toLowerCase());
+    return {
+      ...token,
+      logo,
+    };
+  });
+}
 
-const DEFAULT_POOL_LIST = [
-  {
-    first: {
-      logo: getAssetLogo("gbex"),
-      name: "Globiance Network",
-      symbol: "GBEX",
-      address: "xdc2049552f4f55b8af48c86ed9866a66e7d2bcd0fc",
-      decimals: 18,
-      feeOnTransfer: true,
-    },
-    second: {
-      logo: getAssetLogo("xdc"),
-      name: "XinFin Network",
-      symbol: "XDC",
-      address: "xdcc039850F937C623024DA66D6dF370022E6F16e30",
-      decimals: 18,
-    },
-    address: "xdc1aF38719EdEFE7D2aE5923198CCeea821968278F",
-  },
-  {
-    first: {
-      logo: getAssetLogo("gbex"),
-      name: "Globiance Network",
-      symbol: "GBEX",
-      address: "xdc2049552f4f55b8af48c86ed9866a66e7d2bcd0fc",
-      decimals: 18,
-      feeOnTransfer: true,
-    },
-    second: {
-      logo: getAssetLogo("eurg"),
-      name: "Euro Stablecoin",
-      symbol: "EURG",
-      address: "xdcb128a44f005cbe7ddaeb1860c16821842895ec7a",
-      decimals: 18,
-    },
-    address: "xdc8CDc4592C046a9A8596698dDE629117D2eb610dE",
-  },
-  {
-    first: {
-      logo: getAssetLogo("srx"),
-      name: "StorX Network",
-      symbol: "TSRX",
-      address: "xdc55e574e7d0256ff50f86ee8cd90e4a8e22aa1f85",
-      decimals: 18,
-    },
-    second: {
-      logo: getAssetLogo("xdc"),
-      name: "XinFin Network",
-      symbol: "XDC",
-      address: "xdcc039850F937C623024DA66D6dF370022E6F16e30",
-      decimals: 18,
-    },
-    address: "xdcb50557db49b3A4FaB063a3E53687696d1a92704F",
-  },
-  {
-    first: {
-      logo: getAssetLogo("srx"),
-      name: "StorX Network",
-      symbol: "TSRX",
-      address: "xdc55e574e7d0256ff50f86ee8cd90e4a8e22aa1f85",
-      decimals: 18,
-    },
-    second: {
-      logo: getAssetLogo("gbex"),
-      name: "Globiance Network",
-      symbol: "GBEX",
-      address: "xdc2049552f4f55b8af48c86ed9866a66e7d2bcd0fc",
-      decimals: 18,
-      feeOnTransfer: true,
-    },
-    address: "xdc5847b719d039F53b464e7B09d71eb5DB0a766deF",
-  },
-];
+function getAllBases() {
+  return fs.readdirSync(BASES_PATH).map((x) => {
+    const token = JSON.parse(
+      fs.readFileSync(path.join(TOKENS_PATH, x)).toString()
+    );
+    const logo = getAssetLogo(token.symbol.toLowerCase());
+    return {
+      ...token,
+      logo,
+    };
+  });
+}
+
+function getAllPools() {
+  return fs.readdirSync(POOLS_PATH).map((x) => {
+    const pool = JSON.parse(
+      fs.readFileSync(path.join(POOLS_PATH, x)).toString()
+    );
+    return {
+      first: getTokenFromSymbol(pool.first),
+      second: getTokenFromSymbol(pool.second),
+      address: pool.address,
+    };
+  });
+}
+
+const x = getAllTokens();
+
+const DEFAULT_TOKEN_LIST = [...getAllTokens()];
+
+const COMMON_BASES = [...getAllBases()];
+
+const DEFAULT_POOL_LIST = [...getAllPools()];
+
+console.log(DEFAULT_POOL_LIST);
 
 module.exports = {
   DEFAULT_TOKEN_LIST,
